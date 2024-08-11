22fac947f35f3299de449be3f84de409e53ba293
Fakhrudeen77 <fakhrudeen77@gmail.com>
2024-08-11 20:57:18 +0530

    Initial Commit

---
+++ JavaPractice/src/com/Testing.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Testing {
+
+	public Testing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Programming");
+		
+		
+
+	}
+
+}
